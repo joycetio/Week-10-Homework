9aab26164ffe6d144e84582efb1f739e8f229f83
Marie Joyce Tio <joycetio@Joyces-MacBook-Pro.local>
2017-04-06 23:14:38 -0700

    added movie-this function

--- liri.js
+++ liri.js
@@ -13,12 +13,11 @@ var client = new twitter({
     access_token_key: keys.twitterKeys.access_token_key,
     access_token_secret: keys.twitterKeys.access_token_secret
 });
-// console.log("new client: ", client);
 
 var command = process.argv[2];
-var songTitle = process.argv[3]
+var userInput = process.argv[3]
 
-//switch-case that will direct which function gets run
+//switch-case that will direct which function runs
 switch (command) {
     case "my-tweets":
         myTweets();
@@ -54,11 +53,11 @@ function myTweets() {
 };
 
 function spotifySong() {
-	//search tracks using spotify & if there's no user input, it automatically shows the search results for "The Sign" by Ace of Base
-    spotify.search({ type: 'track', query: songTitle || "The Sign Ace of Base"}, function(err, data) {
+    //search tracks using spotify & if there's no user input, it automatically shows the search results for "The Sign" by Ace of Base
+    spotify.search({ type: 'track', query: userInput || "The Sign Ace of Base" }, function(err, data) {
         var songData = data.tracks.items;
 
- 		//just in case of an error
+        //just in case of an error
         if (err) {
             console.log(err);
         };
@@ -67,7 +66,7 @@ function spotifySong() {
         if (songData.length > 0) {
             var firstResult = songData[0];
             console.log("----------");
-            console.log("YOU SEARCHED FOR " + songTitle);
+            console.log("YOU SEARCHED FOR " + userInput);
             console.log("Artist: " + firstResult.artists[0].name);
             console.log("Song Name: " + firstResult.name);
             console.log("Preview Link of Song: " + firstResult.preview_url);
@@ -77,4 +76,28 @@ function spotifySong() {
             console.log("No songs found! Try another song.");
         }
     });
-}
+};
+
+function movieThis() {
+    request('http://www.omdbapi.com/?t=' + (userInput || "Mr. Nobody") + "&tomatoes=true", function(error, response, body) {
+        if (!error) {
+            //parses through the data received
+            var movieData = JSON.parse(body);
+
+            //logs all the needed info to terminal
+            console.log("-----MOVIE INFO-----")
+            console.log("Title: " + movieData.Title);
+            console.log("Year: " + movieData.Year);
+            console.log("IMDB Rating: " + movieData.imdbRating);
+            console.log("Country: " + movieData.Country);
+            console.log("Language: " + movieData.Language);
+            console.log("Plot: " + movieData.Plot);
+            console.log("Actors: " + movieData.Actors);
+            console.log("Rotten Tomatoes Rating: " + movieData.tomatoRating);
+            console.log("Rotten Tomatoes URL: " + movieData.tomatoURL);
+            console.log("---------------------");
+        } else {
+            console.log("Error occurred: ", error);
+        };
+    });
+};
